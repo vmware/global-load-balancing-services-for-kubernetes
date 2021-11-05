d13066d9b74e80c394809015cbf92dfe9935e99e
Hemant Kumar Shaw <hemant.shaw@avinetworks.com>
2021-11-05 17:02:02 +0530

    Add error details for panic

--- gslb/ingestion/gslb.go
+++ gslb/ingestion/gslb.go
@@ -677,7 +677,7 @@ func Initialize() {
 		// No need to save the Pod metadata, if running AMKO locally.
 		pod, err := kubeClient.CoreV1().Pods(gslbutils.AVISystem).Get(context.TODO(), os.Getenv("POD_NAME"), metav1.GetOptions{})
 		if err != nil {
-			gslbutils.LogAndPanic("Error getting AMKO pod details.")
+			gslbutils.LogAndPanic("error getting AMKO pod details: " + err.Error())
 		}
 		amkoControlConfig.SaveAMKOPodObjectMeta(pod.DeepCopy())
 	}
