865afa79baf9003ee01973cf1eadb275b88ecbd4
Swathin Sankaran <swathins@vmware.com>
2022-02-08 23:50:10 +0530

    This commit contains the following changes:
    1. Moves the MCI, SI and clusterset CRDs to AKO
    2. Integrates the service discovery container to the amko pod
    3. Clusterrole changes for the new CRDs
    4. Makefile changes and new dockerfile for the creation of service discovery container

--- service_discovery/service_import/event_handlers.go
+++ service_discovery/service_import/event_handlers.go
@@ -15,21 +15,21 @@
 package serviceimport
 
 import (
-	// mciapi "github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/apis/amko/v1alpha1"
+	// mciapi "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/apis/amko/v1alpha1"
 	"context"
 	"encoding/json"
 	"fmt"
 
 	"github.com/golang/glog"
 	"github.com/vmware/global-load-balancing-services-for-kubernetes/gslb/gslbutils"
-	sics "github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/client/v1alpha1/clientset/versioned"
 	"github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/client/v1alpha2/clientset/versioned/scheme"
+	sics "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha1/clientset/versioned"
 
-	siapi "github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/apis/amko/v1alpha1"
-	sischeme "github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/client/v1alpha1/clientset/versioned/scheme"
-	siinformers "github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/client/v1alpha1/informers/externalversions"
-	silisters "github.com/vmware/global-load-balancing-services-for-kubernetes/pkg/client/v1alpha1/listers/amko/v1alpha1"
 	k8sutils "github.com/vmware/global-load-balancing-services-for-kubernetes/service_discovery/k8s_utils"
+	siapi "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/apis/ako/v1alpha1"
+	sischeme "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha1/clientset/versioned/scheme"
+	siinformers "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha1/informers/externalversions"
+	silisters "github.com/vmware/load-balancer-and-ingress-services-for-kubernetes/pkg/client/v1alpha1/listers/ako/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -57,7 +57,7 @@ var siController *ServiceImportController
 func InitializeServiceImportController(kubeClient *kubernetes.Clientset, siClient *sics.Clientset,
 	siInformerFactory siinformers.SharedInformerFactory) *ServiceImportController {
 
-	siInformer := siInformerFactory.Amko().V1alpha1().ServiceImports()
+	siInformer := siInformerFactory.Ako().V1alpha1().ServiceImports()
 	// create event broadcaster
 	sischeme.AddToScheme(sischeme.Scheme)
 	eventBroadcaster := record.NewBroadcaster()
@@ -163,7 +163,7 @@ func (siController *ServiceImportController) UpdateServiceImportObj(obj *siapi.S
 		return fmt.Errorf("error in marshalling spec for service import: %v", err)
 	}
 
-	if _, err := siController.siClientset.AmkoV1alpha1().ServiceImports(obj.GetNamespace()).Patch(context.TODO(),
+	if _, err := siController.siClientset.AkoV1alpha1().ServiceImports(obj.GetNamespace()).Patch(context.TODO(),
 		obj.GetName(), types.MergePatchType, patchPayload, v1.PatchOptions{}); err != nil {
 		return fmt.Errorf("error in patching service import object: %v", err)
 	}
@@ -171,7 +171,7 @@ func (siController *ServiceImportController) UpdateServiceImportObj(obj *siapi.S
 }
 
 func (siController *ServiceImportController) CreateServiceImportObj(obj *siapi.ServiceImport) error {
-	if _, err := siController.siClientset.AmkoV1alpha1().ServiceImports(obj.GetNamespace()).Create(context.TODO(),
+	if _, err := siController.siClientset.AkoV1alpha1().ServiceImports(obj.GetNamespace()).Create(context.TODO(),
 		obj, v1.CreateOptions{}); err != nil {
 		return fmt.Errorf("error in creating service import object: %v", err)
 	}
@@ -179,7 +179,7 @@ func (siController *ServiceImportController) CreateServiceImportObj(obj *siapi.S
 }
 
 func (siController *ServiceImportController) DeleteServiceImportObject(ns, name string) error {
-	if err := siController.siClientset.AmkoV1alpha1().ServiceImports(ns).Delete(context.TODO(),
+	if err := siController.siClientset.AkoV1alpha1().ServiceImports(ns).Delete(context.TODO(),
 		name, v1.DeleteOptions{}); err != nil {
 		return fmt.Errorf("error in deleting service import objet: %v", err)
 	}
