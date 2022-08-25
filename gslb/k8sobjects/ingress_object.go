123a5a510cf677bfcffae881d2936e2d25915893
Swathin Sankaran <swathins@vmware.com>
2022-08-25 12:22:03 +0530

    This commit adds the following changes: 1. Reduces the log level of the log from error to info which is logged    when VS and controller UUID is not present in the ingress and    multi-cluster ingress objects. 2. Documents the AMKO's UUID behavior during re-installation. 3. Few bug fixes.

--- gslb/k8sobjects/ingress_object.go
+++ gslb/k8sobjects/ingress_object.go
@@ -130,7 +130,7 @@ func GetIngressHostMeta(ingress *networkingv1.Ingress, cname string) []IngressHo
 		// Note that the ingress key will still be published to graph layer, but the key
 		// won't be processed, this is just to maintain the ingress information as part
 		// of in-memory map.
-		gslbutils.Errf("ns: %s, ingress: %s, msg: skipping ingress because of error: %v",
+		gslbutils.Logf("ns: %s, ingress: %s, msg: skipping ingress because of error: %v",
 			ingress.Namespace, ingress.Name, err)
 	}
 	if (controllerUUID == "" || len(vsUUIDs) == 0) && !syncVIPsOnly {
