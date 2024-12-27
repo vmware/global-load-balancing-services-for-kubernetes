b8cfeb53a7b8a4ae0a3a4f1a631a67fc630bd453
Jeya Pradeen <jeya.pradeen@broadcom.com>
2024-12-27 10:23:07 +0000

    AV-226253: Move amko helm chart files to amko directory inside buildpath

--- amko_cicd/jenkins/ci-build/save_build_on_mnt.sh
+++ amko_cicd/jenkins/ci-build/save_build_on_mnt.sh
@@ -22,26 +22,25 @@ version_build_num=$(expr "$base_build_num" + "$BUILD_NUMBER")
 branch_version=$(bash $BRANCH_VERSION_SCRIPT)
 
 BUILD_VERSION_SCRIPT=$WORKSPACE/hack/jenkins/get_build_version.sh
-CHARTS_PATH="$(get_git_ws)/helm/amko"
+AMKO_CHARTS_PATH="$(get_git_ws)/helm/amko"
 
 build_version=$(bash $BUILD_VERSION_SCRIPT "dummy" $BUILD_NUMBER)
 
 target_path=/mnt/builds/amko_OS/$BRANCH/ci-build-$build_version
+amko_target_path=$target_path/amko
 
 sudo mkdir -p $target_path
+sudo mkdir -p $amko_target_path
 
-sudo cp -r $CHARTS_PATH/* $target_path/
+# Copy amko helm charts
+sudo cp -r $AMKO_CHARTS_PATH/* $amko_target_path/
+sudo sed -i --regexp-extended "s/^(\s*)(appVersion\s*:\s*latest\s*$)/\1appVersion: $build_version/" $amko_target_path/Chart.yaml
 
-set +e
 sudo cp "$(get_git_ws)/HEAD_COMMIT" $target_path/
-
 if [ "$?" != "0" ]; then
     echo "ERROR: Could not save the head commit file into target path"
 fi
 
-set -e
-
-sudo sed -i --regexp-extended "s/^(\s*)(appVersion\s*:\s*latest\s*$)/\1appVersion: $build_version/" $target_path/Chart.yaml
 
 #Save ako images as tarball
 sudo docker save -o amko.tar amko:latest
