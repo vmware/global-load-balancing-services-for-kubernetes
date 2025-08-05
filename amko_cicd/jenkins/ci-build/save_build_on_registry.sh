c42ce5657fc0a94e99ac668f73bf177615967e98
Jeya Pradeen <jpradeen@vmware.com>
2025-08-05 17:31:30 +0530

    AV-245641: Convert branch name to lowercase for docker tag/push

--- amko_cicd/jenkins/ci-build/save_build_on_registry.sh
+++ amko_cicd/jenkins/ci-build/save_build_on_registry.sh
@@ -11,7 +11,7 @@ echo ${AMKO_IMAGES[@]}
 for image in "${AMKO_IMAGES[@]}"
 do
   source_image=$image:latest
-  target_image=$PVT_DOCKER_REGISTRY/$PVT_DOCKER_REPOSITORY/amko/$branch/$image:$version_tag
+  target_image=$PVT_DOCKER_REGISTRY/$PVT_DOCKER_REPOSITORY/amko/${branch,,}/$image:$version_tag
   sudo docker tag $source_image $target_image
   sudo docker push $target_image
 done
